301d2ea4d68591cdd703bb0446b12d5ffc594229
kaneera <ukaneera@Gmail.com>
2022-06-15 11:09:18 -0400

    Initial Commit

---
+++ EcoFocus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EcoFocus
+//
+//  Created by Kaneera on 2022-06-15.
+//  Copyright © 2022 Kaneera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
