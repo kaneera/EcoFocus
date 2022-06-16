c1fb23096492764589f01198ffcb69853f54e5a4
Erica T <my1ngan111@gmail.com>
2022-06-16 10:53:15 -0500

    Complete Iteration 0

---
+++ EcoFocus/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  EcoFocus
+//
+//  Created by blub on 6/16/22.
+//  Copyright © 2022 Kaneera. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ EcoFocus/viewcontrollers/ToDoTableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ToDoTableViewController.swift
+//  EcoFocus
+//
+//  Created by blub on 6/16/22.
+//  Copyright © 2022 Kaneera. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    var toDos : [ToDo] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        toDos = createToDos()
+        }
+    
+        func createToDos() -> [ToDo] {
+
+          let swift = ToDo()
+          swift.name = "Drink water"
+          swift.important = true
+
+          let dog = ToDo()
+          dog.name = "Read a book"
+          // important is set to false by default
+
+          return [swift, dog]
+        }
+        
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return toDos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+      let toDo = toDos[indexPath.row]
+
+      if toDo.important {
+        cell.textLabel?.text = "❗️" + toDo.name
+      } else {
+        cell.textLabel?.text = toDo.name
+      }
+
+      return cell
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+
+
+}
