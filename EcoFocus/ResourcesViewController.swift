5945237b52a16d54876075d7f6f2f398bf81a7aa
kaneera <ukaneera@Gmail.com>
2022-06-17 01:47:45 -0400

    added some buttons to resources page

---
+++ EcoFocus/ResourcesViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ResourcesViewController.swift
+//  EcoFocus
+//
+//  Created by Kaneera on 2022-06-17.
+//  Copyright © 2022 Kaneera. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ResourcesViewController: UIViewController {
+    
+    @IBAction func placesToDonate(_ sender: Any) {
+    }
+    
+    @IBAction func organizations(_ sender: Any) {
+    }
+    
+    @IBAction func climate101(_ sender: Any) {
+    }
+    
+    @IBAction func ecoFriendly(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var myWebView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let googleURL = URL (string: "")
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
